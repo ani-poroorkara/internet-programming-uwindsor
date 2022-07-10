840d82e038a7ed63b15b8d32a4314bb4f9eb4e0c
Vatsal Modh <59243112+Vatsal221@users.noreply.github.com>
2022-07-10 14:02:12 -0400

    Updated forms.py

--- Lab 2/mywebapp22/myapp/forms.py
+++ Lab 2/mywebapp22/myapp/forms.py
@@ -6,3 +6,14 @@ class InterestForm(forms.Form):
     interested = forms.CharField(widget=forms.RadioSelect(choices=CHOICES))
     levels = forms.IntegerField(initial = 1)
     comments = forms.CharField(label='Additional Comments', required=False, widget=forms.Textarea)
+    
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order;
+        fields = ['Student', 'course', 'levels', 'order_date']
+        widgets = {
+            'student': forms.RadioSelect
+        }
+        widgets = {
+            'order_date': forms.SelectDateWidget
+        }
