86303e64a9a2e5947f45b4d58777a2eb6cc52787
Vatsal Modh <59243112+Vatsal221@users.noreply.github.com>
2022-07-17 21:33:18 -0400

    lab9 updates

--- Lab 2/mywebapp22/myapp/urls.py
+++ Lab 2/mywebapp22/myapp/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
  path(r'courses', views.courses, name='courses'),
  path(r'detail/<id>/', views.detail, name='detail'),
  path(r'place_order', views.place_order, name='placeOrder'),
- path(r'courses/<int:cour_id>/', views.coursedetail, name='courseDetail')
+ path(r'courses/<int:cour_id>/', views.coursedetail, name='courseDetail'),
+ path(r'user_login', views.user_login, name='user_login'),
+ path(r'user_logout', views.user_logout, name='user_logout'),
+ path(r'my_account', views.myaccount, name='myaccount')
 ]
