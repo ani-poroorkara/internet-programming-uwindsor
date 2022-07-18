b8f554a7842222fcf5560a3bd19ee2e4269da711
Vatsal Modh <59243112+Vatsal221@users.noreply.github.com>
2022-07-17 21:32:40 -0400

    lab9 updates

--- Lab 2/mywebapp22/myapp/views.py
+++ Lab 2/mywebapp22/myapp/views.py
@@ -1,24 +1,40 @@
 from unicodedata import category
 from urllib import response
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Topic, Course, Student, Order
+from datetime import datetime
 
 from .forms import *
 
 # Create your views here.
 def index(request):
     top_list = Topic.objects.all().order_by('id')[:10]
-    return render(request, 'myapp/index.html', {'top_list': top_list})
+    if 'last_login' in request.session:
+        last_login = request.session['last_login']
+    else:
+        last_login = 'Your last login was more than one hour ago'
+    return render(request, 'myapp/index.html', {'top_list': top_list, 'last_login': last_login})
+    #return render(request, 'myapp/index.html', {'top_list': top_list})
 
 def courses(request):
     courlist = Course.objects.all().order_by('id')
     return render(request, 'myapp/courses.html', {'courlist': courlist})
 
 def about(request):
-    return render(request, 'myapp/about.html')
+    if 'about_visits' in request.session:
+        request.session['about_visits'] += 1
+    else:
+        request.session['about_visits'] = 1
+        request.session.set_expiry(300)
+    return render(request, 'myapp/about.html', {'about_visits': request.session['about_visits']})
+    #return render(request, 'myapp/about.html')
 
 def detail(request, id):
     response = HttpResponse()
@@ -67,3 +83,50 @@ def coursedetail(request, cour_id):
     else:
         form = InterestForm()
         return render(request, 'myapp/coursedetail.html', {'form': form, 'course': course})
+    
+def user_login(request):
+    if request.method == 'POST':
+        if request.session.test_cookie_worked():
+            request.session.delete_test_cookie()
+            print("test cookie worked")
+        else:
+            print("test cookie didn't work")
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                request.session['last_login'] = datetime.now().strftime("%H:%M:%S - %B %d, %Y")
+                # request.session.set_expiry(3600)
+                request.session.set_expiry(0)
+                return HttpResponseRedirect(reverse('myapp:index'))
+            else:
+                return HttpResponse('Your account is disabled.')
+        else:
+            return HttpResponse('Invalid login details.')
+    else:
+        request.session.set_test_cookie()
+        return render(request, 'myapp/login.html')
+
+
+@login_required
+def user_logout(request):
+    # logout(request)
+    # request.session.flush()
+    for key in list(request.session.keys()):
+        del request.session[key]
+    return HttpResponseRedirect(reverse('myapp:index'))
+
+
+@login_required
+def myaccount(request):
+    students = Student.objects.filter(pk=request.user.id)
+    if len(students) == 1:
+        student = students[0]
+        return render(request, 'myapp/my_account.html', {'fullName': student.first_name + " " + student.last_name,
+                                                         'orders': student.orders.all(),
+                                                         'interested_in': student.interested_in.all()
+                                                         })
+    else:
+        return HttpResponse('You are not a registered student!')
